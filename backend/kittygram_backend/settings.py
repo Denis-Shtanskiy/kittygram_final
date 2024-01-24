a6aa1d66a6dae2e7369dd485c3d408f52a804e8d
Denis Shtanskiy <denis_shtanskii@bk.ru>
2024-01-24 14:55:26 +0300

    fix Debug on databases

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
-if os.getenv('DEBUG').lower() == 'true':
+if os.getenv('DEBUG') == 'True':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
